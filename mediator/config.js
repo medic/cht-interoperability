5f7785103de50bf05d8587d7ef8fd0842106b11d
Samuel Imoisili <samuelomohan@gmail.com>
2023-01-11 08:42:22 +0000

    added new line to files, eslint rule and removed .env

--- mediator/config.js
+++ mediator/config.js
@@ -1,11 +1,11 @@
-require("dotenv/config")
+require('dotenv/config');
 
 module.exports = {
-    PORT: process.env.PORT || 5001,
-    OPENHIM: {
-        username: process.env.OPENHIM_USERNAME,
-        password: process.env.OPENHIM_PASSWORD,
-        apiURL: process.env.OPENHIM_API_URL ,
-        trustSelfSigned: true,
-    }
-}
+  PORT: process.env.PORT || 5001,
+  OPENHIM: {
+    username: process.env.OPENHIM_USERNAME,
+    password: process.env.OPENHIM_PASSWORD,
+    apiURL: process.env.OPENHIM_API_URL,
+    trustSelfSigned: true
+  }
+};
