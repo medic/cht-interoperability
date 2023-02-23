69a4211ff55a94fa04204ae4b7e41dac6f248c60
Njuguna Ndung'u <njgndungu@yahoo.com>
2023-02-23 18:49:18 +0300

    update validation code

--- mediator/src/middlewares/schemas/service-request.ts
+++ mediator/src/middlewares/schemas/service-request.ts
@@ -1,6 +1,6 @@
 import joi from 'joi';
 
 export const createServiceSchema = joi.object({
-  patient_id: joi.number().required(),
-  callbackURL: joi.string().uri().required(),
+  patient_id: joi.string().required(),
+  callback_url: joi.string().uri().required(),
 });
