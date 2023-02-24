8f505e827f9559ab71728ec24488e3a4cb3702e7
Njuguna Ndung'u <njgndungu@yahoo.com>
2023-02-24 16:52:26 +0300

    add jest config and sample test file (#35)
    Add jest config and sample test file

---
+++ mediator/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+  ],
+};
