402fd9ffa52ae7e936273d935b5565d3290adb5b
Samuel Imoisili <samuelomohan@gmail.com>
2023-03-02 09:20:46 +0000

    fixed bad request body

--- mediator/src/middlewares/index.ts
+++ mediator/src/middlewares/index.ts
@@ -1,10 +1,13 @@
-import { Request, Response, NextFunction } from 'express';
-import { ValidationError } from 'joi';
+import { Request, Response, NextFunction } from "express";
+import { ValidationError } from "joi";
 
 export const validateBodyAgainst = (schema: any) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    schema.validateAsync(req.body, { abortEarly: true })
+    schema
+      .validateAsync(req.body, { abortEarly: true })
       .then(() => next())
-      .catch(({ details }: ValidationError) => res.status(400).send(details[0]));
+      .catch(({ details }: ValidationError) =>
+        res.status(400).send({ message: details[0].message })
+      );
   };
-}
+};
