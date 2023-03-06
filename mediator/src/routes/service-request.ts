10c567cf461d03141215ce002f327d599f20f816
Njuguna Ndung'u <njgndungu@yahoo.com>
2023-03-06 14:52:18 +0300

    address PR comments

--- mediator/src/routes/service-request.ts
+++ mediator/src/routes/service-request.ts
@@ -1,15 +1,17 @@
-import { Request } from 'express';
-import {validateBodyAgainst} from '../middlewares';
-import {requestHandler} from '../utils/service-request';
+import { Request } from "express";
+import { validateBodyAgainst } from "../middlewares";
+import { requestHandler } from "../utils/service-request";
 
-const {Router} = require('express');
-const {createServiceRequest} = require('../controllers/service-request');
-const {createServiceSchema} = require('../middlewares/schemas/service-request');
+const { Router } = require("express");
+const { createServiceRequest } = require("../controllers/service-request");
+const {
+  createServiceSchema,
+} = require("../middlewares/schemas/service-request");
 
 const router = Router();
 
 router.post(
-  '/',
+  "/",
   validateBodyAgainst(createServiceSchema),
   requestHandler((req: Request) => createServiceRequest(req.body))
 );
