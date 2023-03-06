a060415f354548394224d637a68a253c99965e0b
Njuguna Ndung'u <njgndungu@yahoo.com>
2023-03-06 14:57:11 +0300

    26 create subscription resource (#28)
    * add util for generating a subscription and a change a few things for debugging purposes
    
    * call the CHT to create a task
    
    * status should be 201 not 200
    
    * enable subscription
    
    * update validation code
    
    * add instructions for saving credentials to the db
    
    * add user creation instructions
    
    * state which role is required

--- mediator/src/middlewares/schemas/service-request.ts
+++ mediator/src/middlewares/schemas/service-request.ts
@@ -1,5 +1,6 @@
 import joi from 'joi';
 
 export const createServiceSchema = joi.object({
-  patient_id: joi.number().required()
+  patient_id: joi.string().required(),
+  callback_url: joi.string().uri().required(),
 });
