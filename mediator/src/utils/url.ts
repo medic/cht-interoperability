53e6c6608875a4edb0b3a53bcb3f3950c919afa0
Samuel Imoisili <samuelomohan@gmail.com>
2023-03-15 17:07:16 +0000

    completed testing url and update the url creation function

--- mediator/src/utils/url.ts
+++ mediator/src/utils/url.ts
@@ -9,7 +9,7 @@ export function generateBasicAuthUrl(endpoint: string, username: string, passwor
   const parsedUrl = url.parse(endpoint);
   const urlParts = [];
 
-  const { protocol, hostname, port } = parsedUrl;
+  const { protocol, hostname, port, path } = parsedUrl;
 
   urlParts.push(`${protocol}//`)
   urlParts.push(`${username}:${password}@`)
@@ -19,13 +19,12 @@ export function generateBasicAuthUrl(endpoint: string, username: string, passwor
     urlParts.push(`:${port}`)
   }
 
+  urlParts.push(path)
+
   return urlParts.join("");
 }
 
 export const generateChtRecordsApiUrl = (chtUrl: string, username: string, password: string) => {
-  // const parsedUrl = url.parse(chtUrl);
-  // const {protocol, hostname} = parsedUrl;
-  // const apiURL = `${protocol}//${username}:${password}@${hostname}/api/v2/records`;
   const endpoint = generateBasicAuthUrl(chtUrl, username, password);
   return path.join(endpoint, "/api/v2/records");
 }
