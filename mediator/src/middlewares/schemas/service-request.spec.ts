75ce7c359013a5db6a1ad5851be02e61e7b4d154
Samuel Imoisili <samuelomohan@gmail.com>
2023-03-16 12:17:40 +0000

    completed unit testing the middlewares

---
+++ mediator/src/middlewares/schemas/service-request.spec.ts
@@ -0,0 +1,30 @@
+import { createServiceSchema } from "./service-request";
+
+describe("createServiceSchema", () => {
+  it("accepts valid values", async () => {
+    const data = {
+      patient_id: "PATIENT_ID",
+      callback_url: "https://google.com/",
+    };
+
+    expect(createServiceSchema.validateAsync(data)).resolves.toMatchSnapshot();
+  });
+
+  it("rejects data with valid 'callback_url'", () => {
+    const data = {
+      patient_id: "PATIENT_ID",
+      callback_url: "INVALID_URL",
+    };
+
+    expect(createServiceSchema.validateAsync(data)).rejects.toMatchSnapshot();
+  });
+
+  it("rejects data with invalid 'patient_id' ", () => {
+    const data = {
+      patient_id: undefined,
+      callback_url: "https://google.com",
+    };
+
+    expect(createServiceSchema.validateAsync(data)).rejects.toMatchSnapshot();
+  });
+});
