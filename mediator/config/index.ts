206badca365e6c828b821a14806ecf29845652bc
Samuel Imoisili <samuelomohan@gmail.com>
2023-03-17 09:48:16 +0000

    added the fix to make all test environment the same

--- mediator/config/index.ts
+++ mediator/config/index.ts
@@ -4,20 +4,29 @@ dotenv.config();
 export const PORT = process.env.PORT || 6000;
 
 export const OPENHIM = {
-  username: process.env.OPENHIM_USERNAME || "interop@openhim.org",
-  password: process.env.OPENHIM_PASSWORD || "interop-password",
-  apiURL: process.env.OPENHIM_API_URL || "https://openhim-core:8080",
+  username: getEnvironmentVariable(process.env.OPENHIM_USERNAME, "interop@openhim.org"),
+  password: getEnvironmentVariable(process.env.OPENHIM_PASSWORD, "interop-password"),
+  apiURL: getEnvironmentVariable(process.env.OPENHIM_API_URL, "https://openhim-core:8080"),
   trustSelfSigned: true,
 };
 
 export const FHIR = {
-  url: process.env.FHIR_URL || "http://openhim-core:5001/fhir",
-  username: process.env.FHIR_USERNAME || "interop-client",
-  password: process.env.FHIR_PASSWORD || "interop-password",
+  url: getEnvironmentVariable(process.env.FHIR_URL, "http://openhim-core:5001/fhir"),
+  username: getEnvironmentVariable(process.env.FHIR_USERNAME, "interop-client"),
+  password: getEnvironmentVariable(process.env.FHIR_PASSWORD, "interop-password"),
 };
 
 export const CHT = {
-  url: process.env.CHT_URL || "https://nginx",
-  username: process.env.CHT_USERNAME || "admin",
-  password: process.env.CHT_PASSWORD || "password",
+  url: getEnvironmentVariable(process.env.CHT_URL, "http://nginx"),
+  username: getEnvironmentVariable(process.env.CHT_USERNAME, "admin"),
+  password: getEnvironmentVariable(process.env.CHT_PASSWORD, "password"),
 };
+
+
+function getEnvironmentVariable(env: string | undefined, def: string) {
+  if (process.env.NODE_ENV === "test") {
+    return def;
+  }
+  
+  return process.env[env as string] || def;
+}
