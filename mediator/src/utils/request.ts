f7efd7c3d46b955f76a472c5d26af8b34fb2610f
Samuel Imoisili <samuelomohan@gmail.com>
2023-03-22 07:22:30 +0000

    resolved conflicts with master

--- mediator/src/utils/request.ts
+++ mediator/src/utils/request.ts
@@ -5,14 +5,8 @@ export type RequestHandler = (
 ) => Promise<{ status: number; data: any }>;
 
 export function requestHandler(handler: RequestHandler) {
-<<<<<<< HEAD
-  return  (req: Request, res: Response) => {
-    handler(req)
-    .then(({status, data}) => {
-=======
   return (req: Request, res: Response) =>
     handler(req).then(({ status = 200, data }) => {
->>>>>>> 2ed59a1 (completed writing tests for FHIR utility)
       res.status(status).send(data);
     });
 }
