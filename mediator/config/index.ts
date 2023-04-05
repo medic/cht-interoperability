48c83453c27a4b6e730401bebb2bb59eb6ec0ee4
Henok <henygetachew@gmail.com>
2023-04-05 11:27:19 +0300

    Fix how we call the getEnvironmentVariable

--- mediator/config/index.ts
+++ mediator/config/index.ts
@@ -4,22 +4,22 @@ dotenv.config();
 export const PORT = process.env.PORT || 6000;
 
 export const OPENHIM = {
-  username: getEnvironmentVariable(process.env.OPENHIM_USERNAME, 'interop@openhim.org'),
-  password: getEnvironmentVariable(process.env.OPENHIM_PASSWORD, 'interop-password'),
-  apiURL: getEnvironmentVariable(process.env.OPENHIM_API_URL, 'https://openhim-core:8080'),
+  username: getEnvironmentVariable('OPENHIM_USERNAME', 'interop@openhim.org'),
+  password: getEnvironmentVariable('OPENHIM_PASSWORD', 'interop-password'),
+  apiURL: getEnvironmentVariable('OPENHIM_API_URL', 'https://openhim-core:8080'),
   trustSelfSigned: true,
 };
 
 export const FHIR = {
-  url: getEnvironmentVariable(process.env.FHIR_URL, 'http://openhim-core:5001/fhir'),
-  username: getEnvironmentVariable(process.env.FHIR_USERNAME, 'interop-client'),
-  password: getEnvironmentVariable(process.env.FHIR_PASSWORD, 'interop-password'),
+  url: getEnvironmentVariable('FHIR_URL', 'http://openhim-core:5001/fhir'),
+  username: getEnvironmentVariable('FHIR_USERNAME', 'interop-client'),
+  password: getEnvironmentVariable('FHIR_PASSWORD', 'interop-password'),
 };
 
 export const CHT = {
-  url: getEnvironmentVariable(process.env.CHT_URL, 'https://nginx'),
-  username: getEnvironmentVariable(process.env.CHT_USERNAME, 'admin'),
-  password: getEnvironmentVariable(process.env.CHT_PASSWORD, 'password'),
+  url: getEnvironmentVariable('CHT_URL', 'https://nginx'),
+  username: getEnvironmentVariable('CHT_USERNAME', 'admin'),
+  password: getEnvironmentVariable('CHT_PASSWORD', 'password'),
 };
 
 
