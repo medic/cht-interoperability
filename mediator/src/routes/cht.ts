7a5243c242ac4f83b4293c638714a910ec372b61
Tom Wier <thomas@medic.org>
2024-06-24 07:34:13 -0700

    feat(#114): fix tests

--- mediator/src/routes/cht.ts
+++ mediator/src/routes/cht.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { requestHandler } from '../utils/request';
 import { createPatient, updatePatientIds, createEncounter } from '../controllers/cht'
-import { syncPatients, syncEncounters } from '../utils/openmrs_sync'
 
 const router = Router();
 
@@ -20,19 +19,6 @@ router.post(
 router.post(
   '/encounter',
   requestHandler((req) => createEncounter(req.body))
-);
-
-router.post(
-  '/sync',
-  requestHandler(async (req) => {
-    async function syncAll() {
-      await syncPatients();
-      await syncEncounters();
-    }
-    // dont await, return immediately
-    syncAll();
-    return { status: 200, data: {}};
-  })
 );
 
 export default router;
