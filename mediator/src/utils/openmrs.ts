86bdc31d7a8453aed3eb5ca9b22d9a2dc538478f
Tom Wier <thomas@medic.org>
2024-10-18 15:08:02 +0300

    chore(#142): adding tests to increase coverage

--- mediator/src/utils/openmrs.ts
+++ mediator/src/utils/openmrs.ts
@@ -2,6 +2,7 @@ import { OPENMRS } from '../../config';
 import axios from 'axios';
 import { logger } from '../../logger';
 import https from 'https';
+import { getResourcesSince } from './fhir';
 
 const axiosOptions = {
   auth: {
@@ -14,41 +15,8 @@ const axiosOptions = {
   timeout: OPENMRS.timeout
 };
 
-export async function getOpenMRSPatientResource(patientId: string) {
-  return await axios.get(
-    `${OPENMRS.url}/Patient/?identifier=${patientId}`,
-    axiosOptions
-  );
-}
-
 export async function getOpenMRSResourcesSince(lastUpdated: Date, resourceType: string) {
-  try {
-    let nextUrl = `${OPENMRS.url}/${resourceType}/?_lastUpdated=gt${lastUpdated.toISOString()}`;
-    let results: fhir4.Resource[] = [];
-    // for encounters, include related resources
-    if (resourceType === 'Encounter') {
-      nextUrl = nextUrl + '&_revinclude=Observation:encounter&_include=Encounter:patient';
-    }
-
-    while (nextUrl) {
-      const res = await axios.get(nextUrl, axiosOptions);
-
-      if (res.data.entry){
-        results = results.concat(res.data.entry.map((entry: any) => entry.resource));
-      }
-
-      const nextLink = res.data.link && res.data.link.find((link: any) => link.relation === 'next');
-      nextUrl = nextLink ? nextLink.url : null;
-      if (nextUrl) {
-        const qs = nextUrl.split('?')[1];
-        nextUrl = `${OPENMRS.url}/?${qs}`;
-      }
-    }
-    return { status: 200, data: results };
-  } catch (error: any) {
-    logger.error(error);
-    return { status: error.status, data: error.data };
-  }
+  return getResourcesSince(OPENMRS.url, lastUpdated, resourceType);
 }
 
 export async function createOpenMRSResource(doc: fhir4.Resource) {
@@ -57,7 +25,7 @@ export async function createOpenMRSResource(doc: fhir4.Resource) {
     return { status: res.status, data: res.data };
   } catch (error: any) {
     logger.error(error);
-    return { status: error.status, data: error.data };
+    return { status: error.response?.status, data: error.response?.data };
   }
 }
 
@@ -67,6 +35,6 @@ export async function updateOpenMRSResource(doc: fhir4.Resource) {
     return { status: res.status, data: res.data };
   } catch (error: any) {
     logger.error(error);
-    return { status: error.status, data: error.data };
+    return { status: error.response?.status, data: error.response?.data };
   }
 }
--- mediator/src/utils/tests/cht.spec.ts
+++ mediator/src/utils/tests/cht.spec.ts
@@ -1,7 +1,15 @@
-import { createChtFollowUpRecord, generateChtRecordsApiUrl } from '../cht';
+import {
+  createChtFollowUpRecord,
+  generateChtRecordsApiUrl,
+  getLocationFromOpenMRSPatient,
+  queryCht } from '../cht';
 import axios from 'axios';
+import { logger } from '../../../logger';
+import { OpenMRSPatientFactory } from '../../middlewares/schemas/tests/openmrs-resource-factories';
+import { mockQueryCht } from '../../controllers/tests/utils';
 
 jest.mock('axios');
+jest.mock('../../../logger');
 
 const mockAxios = axios as jest.Mocked<typeof axios>;
 
@@ -34,6 +42,88 @@ describe('CHT Utils', () => {
       expect(res).toContain('cht');
       expect(res).toContain('8000');
       expect(res).toContain(`${username}:${password}`);
+    });
+  });
+
+  describe('getLocationFromOpenMRSPatient', () => {
+    it('should return place ID if address contains place ID', async () => {
+      const fhirPatient = OpenMRSPatientFactory.build({}, {
+        addressKey: 'address4',
+        addressValue: 'FCHV Area [12345]'
+      });
+
+      const result = await getLocationFromOpenMRSPatient(fhirPatient as any);
+
+      expect(result).toBe('12345');
+    });
+
+    it('should return an empty string if no address or place ID is found', async () => {
+      const fhirPatient = OpenMRSPatientFactory.build({}, {
+        addressKey: 'address4',
+        addressValue: 'Unknown Area'
+      });
+
+      mockQueryCht.mockResolvedValue({ status: 200, data: { docs: [] } }); // Simulating no result from the query
+
+      const result = await getLocationFromOpenMRSPatient(fhirPatient as any);
+
+      expect(result).toBe('');
+    });
+
+    it('should return address5 if address4 is not available', async () => {
+      const fhirPatient = OpenMRSPatientFactory.build({}, {
+        addressKey: 'address5',
+        addressValue: 'Health Center [54321]'
+      });
+
+      const result = await getLocationFromOpenMRSPatient(fhirPatient as any);
+
+      expect(result).toBe('54321');
+    });
+
+    it('should handle error cases by returning an empty string when query fails', async () => {
+      const fhirPatient = OpenMRSPatientFactory.build({}, {
+        addressKey: 'address4',
+        addressValue: 'Unknown Location'
+      });
+
+      mockQueryCht.mockRejectedValue(new Error('Database query failed'));
+
+      const result = await getLocationFromOpenMRSPatient(fhirPatient as any);
+
+      expect(result).toBe('');
+    });
+  });
+
+  describe('queryCHT', () => {
+    it('should return data when the query is successful', async () => {
+      const mockQuery = { selector: { type: 'contact' } };
+      const mockResponse = { status: 200, data: { docs: [{ place_id: '12345' }] } };
+
+      mockAxios.post.mockResolvedValue(mockResponse); // Simulate a successful response
+
+      const result = await queryCht(mockQuery);
+
+      expect(mockAxios.post).toHaveBeenCalledWith(expect.stringContaining('_find'), mockQuery, expect.anything());
+      expect(result).toEqual(mockResponse);
+    });
+
+    it('should log an error and return error.response.data when the query fails', async () => {
+      const mockQuery = { selector: { type: 'contact' } };
+      const mockError = {
+        response: { status: 500, data: 'Internal Server Error' }
+      }
+
+      mockAxios.post.mockRejectedValue(mockError); // Simulate an error response
+      const loggerErrorSpy = jest.spyOn(logger, 'error'); // Spy on the logger's error method
+
+      const result = await queryCht(mockQuery);
+
+      expect(loggerErrorSpy).toHaveBeenCalledWith(mockError);
+      expect(result).toEqual({
+        status: mockError.response.status,
+        data: mockError.response.data
+      });
     });
   });
 });
