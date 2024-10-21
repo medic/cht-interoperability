4ff49ac98d75193e375a5c077741871739fb9057
Njuguna Ndung'u <njuguna@medic.org>
2024-10-21 07:37:55 +0300

    chore: try adding default config and update startup script

--- mediator/config/openmrs_mediator.ts
+++ mediator/config/openmrs_mediator.ts
@@ -3,10 +3,29 @@ export const openMRSMediatorConfig = {
   version: '1.0.0',
   name: 'OpenMRS Mediator',
   description: 'A mediator to sync CHT data with OpenMRS',
+  defaultChannelConfig: [
+    {
+      name: 'OpenMRS Mediator',
+      urlPattern: '^/openmrs/.*$',
+      routes: [
+        {
+          name: 'OpenMRS Mediator',
+          host: 'mediator',
+          pathTransform: 's/\\/openmrs/',
+          port: 6000,
+          primary: true,
+          type: 'http',
+        },
+      ],
+      allow: ['interop'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      type: 'http',
+    },
+  ],
   endpoints: [
     {
       name: 'OpenMRS Mediator',
-      host: 'mediator',
+      host: 'openmrs',
       path: '/',
       port: '6000',
       primary: true,
